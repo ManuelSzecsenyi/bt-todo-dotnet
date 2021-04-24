ab1633e5b7148847db0758a52fa220009df9ec66
Manuel <manuel.szecsenyi@gmail.com>
2021-04-24 17:45:41 +0200

    fixes row margin added dummy todo

--- todoXamarinForms/MainViewModel.cs
+++ todoXamarinForms/MainViewModel.cs
@@ -1,19 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Reflection;
 using System.Text;
+using todoXamarinForms.Models;
 using Xamarin.Forms;
 
 namespace todoXamarinForms
 {
-    public class MainViewModel : BindableObject
+    public class MainViewModel : BindableObject, INotifyPropertyChanged
     {
 
-        public string CurrentDate { get; set; }  //DateTime.Now.ToString("dd'/'MM'/'yyyy HH:mm:ss");
+        public string CurrentDate { get; set; }  
+        public ObservableCollection<TodoItem> TodoList { get; set; }
 
 
         public MainViewModel()
         {
             this.CurrentDate = DateTime.Now.ToString("ddd dd.MM.yy");
+
+            this.TodoList = new ObservableCollection<TodoItem>();
+
+            this.TodoList.Add(new TodoItem("Sauber machen", false));
+            this.TodoList.Add(new TodoItem("Sauber Nummer zwei", false));
         }
     }
 }
