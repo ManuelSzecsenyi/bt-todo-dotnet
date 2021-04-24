737ee4736de57420a51c02197b3ebf3d2af7f95c
Manuel <manuel.szecsenyi@gmail.com>
2021-04-24 16:03:10 +0200

    view model created

---
+++ todoXamarinForms/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace todoXamarinForms
+{
+    public class MainViewModel : BindableObject
+    {
+
+        public string CurrentDate { get; set; }  //DateTime.Now.ToString("dd'/'MM'/'yyyy HH:mm:ss");
+
+
+        public MainViewModel()
+        {
+            this.CurrentDate = DateTime.Now.ToString("ddd dd.MM.yy");
+        }
+    }
+}
