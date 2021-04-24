bb7ba3e2a26927eb344ab06abef52364b530495c
Manuel <manuel.szecsenyi@gmail.com>
2021-04-24 17:45:41 +0200

    added dummy todo

---
+++ todoXamarinForms/TodoItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace todoXamarinForms.Models
+{
+    public class TodoItem
+    {
+        public string Text { get; set; }
+        public bool Done { get; set; }
+
+        public TodoItem(string text, bool done)
+        {
+            this.Text = text;
+            this.Done = done;
+        }
+
+    }
+}
