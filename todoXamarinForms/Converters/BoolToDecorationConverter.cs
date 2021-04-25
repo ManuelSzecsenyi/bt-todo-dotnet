c3bd89516cd37bc16805647f619f1d79946d010f
Manuel <manuel.szecsenyi@gmail.com>
2021-04-25 12:24:21 +0200

    added text decoration converter

---
+++ todoXamarinForms/Converters/BoolToDecorationConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace todoXamarinForms.Converters
+{
+    class BoolToDecorationConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool boolValue = (bool)value;
+
+            if (boolValue)
+            {
+                return TextDecorations.None;
+            } else
+            {
+                return TextDecorations.Strikethrough;
+            }
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
